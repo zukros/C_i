b6a801e0ccfd2689712920bd4622ce67b07b585e
Akshay Sankle <32200419+zukros@users.noreply.github.com>
2020-10-01 17:16:43 +0530

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/C_i.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_i.dir/hello.c.obj"
+  "C_i.exe"
+  "C_i.exe.manifest"
+  "C_i.pdb"
+  "libC_i.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_i.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ hello.c
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 10/1/2020.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int testinteger;
+    printf("Enter a integer:");
+    scanf("%d", &testinteger);
+    printf("Number=%d", testinteger);
+    return 0;
+}
